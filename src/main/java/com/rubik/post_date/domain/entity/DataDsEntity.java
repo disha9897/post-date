26c3240c45443057c394d2009ff63a5460f11c03
disha9897 <dishachauhan500@gmail.com>
2025-01-02 18:19:01 +0530

    Increase String length

--- src/main/java/com/rubik/post_date/domain/entity/DataDsEntity.java
+++ src/main/java/com/rubik/post_date/domain/entity/DataDsEntity.java
@@ -18,22 +18,47 @@ public class DataDsEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+
+    @Column(length = 4000)
     private String status;
+
+    @Column(length = 4000)
     private String error;
+
+    @Column(length = 4000)
     private String errorCode;
+
+    @Column(length = 4000)
     private String eWayBillNo;
+
+    @Column(length = 4000)
     private String cEWB;
 
-    @Column(name = "input_date")
+    @Column(name = "input_date", length = 4000)
     private String date;
 
+    @Column(length = 4000)
     private String validUpTo;
+
+    @Column(length = 4000)
     private String gSTIN;
+
+    @Column(length = 4000)
     private String docNo;
+
+    @Column(length = 4000)
     private String uniqueKey;
+
+    @Column(length = 4000)
     private String eWBDetails;
+
+    @Column(length = 4000)
     private String alert;
+
+    @Column(length = 4000)
     private String info;
+
+    @Column(length = 4000)
     private String userDate;
 
 }
